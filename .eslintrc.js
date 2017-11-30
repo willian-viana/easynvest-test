443e628c279d507802e06f3b270c165870b9200e
Willian Viana <willian.batista.viana@gmail.com>
2017-11-30 19:53:48 -0200

    added ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'node': true,
+    'es6': true
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+    "comma-dangle": ["error", "never"],
+  }
+}
